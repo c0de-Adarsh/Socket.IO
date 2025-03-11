9e694ee8aabaf9df8a0a8f08031e93111f51d925
Adarsh <152369806+c0de-Adarsh@users.noreply.github.com>
2025-03-11 12:25:14 +0530

    backend error fix

--- Backend/Server.js
+++ Backend/Server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.json({ limit: "10mb" })); 
 app.use(cookieParser())
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'*',
   credentials:true
 })) 
  app.use(router)
@@ -22,7 +22,7 @@ app.use(cors({
  const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true
   }
